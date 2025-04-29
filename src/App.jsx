2eca65571d8dce64d2b80379a275f067ba8f6e89
Durubhuru14 <durubhuru01@gmail.com>
2025-04-29 08:32:34 +0530

    Changes: Main content height to vh to 100dvh

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { SearchProvider } from "./context/SearchContext";
 
 function App() {
   return (
-    <main className="bg-gray-100 dark:bg-gray-900 h-screen">
+    <main className="bg-gray-100 dark:bg-gray-900 h-dvh">
       <SearchProvider>
         <Navbar />
         <Gallery />
